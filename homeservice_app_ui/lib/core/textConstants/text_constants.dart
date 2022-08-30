78f72cced7e0e576924cfaac5fd5935fbb05e93e
mobisa-012 <mobisakwamboka@gmail.com>
2022-08-30 10:11:53 +0300

    colors files

---
+++ homeservice_app_ui/lib/core/textConstants/text_constants.dart
@@ -0,0 +1,30 @@
+class TextConstants {
+  static const String prepareCleaning = 'Prepare Cleaning';
+  static const String otherServices = 'Other Services';
+  static const String otherServicesCarService = 'Vehicle';
+  static const String otherServicesPainting = 'Painting';
+  static const String otherServicesLabourService = 'Labour Service';
+  static const String allCategory = 'All Category';
+  static const String allCategory1 = 'Commercial and Shifting';
+  static const String allCategory2 = 'Car and Vehicles';
+  static const String allCategory3 = 'Office Shifting';
+  static const String allCategory4 = 'Home Shifting';
+  static const String welcome = 'Welcome';
+  static const String selectType = 'Select type';
+  static const String viewFullProfile = 'View Full Profile';
+  static const String cleaningTime = 'Cleaning Time';
+  static const String furnitures = 'Furnitures';
+  static const String furnitures1 = 'Bed';
+  static const String furnitures2 = 'Sofa';
+  static const String furnitures3 = 'Chair';
+  static const String furnitures4 = 'AC';
+  static const String orderDetails = 'Order Details';
+  static const String itemPageTitle = 'Item';
+  static const String confitmButtonText = 'Confirm';
+  static const String roomsNumber = 'Rooms in your house';
+  static const int roomsNumber1 = 1;
+  static const int roomsNumber2 = 2;
+  static const int roomsNumber3 = 3;
+  static const int roomsNumber4 = 4;
+  static const int roomsNumber5 = 5;
+}
