43ffc7eeffdf6c1bfe2da9d5edb7739de512e2ee
mobisa-012 <mobisakwamboka@gmail.com>
2022-08-30 09:45:16 +0300

    add app's directories

---
+++ homeservice_app_ui/lib/core/const/colors/color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static Color homeBackground = const Color(0xFFffffff);
+  static Color selectRoomColorActive = const Color.fromRGBO(235, 57, 155, 1);
+  static Color selectRoomColorInactive =const Color.fromARGB(255, 241, 135, 188);
+  static Color cleaningService = const Color.fromARGB(255, 14, 245, 26);
+  static Color paintingService = const Color.fromARGB(255, 241, 19, 19);
+  static Color carService = const Color.fromARGB(255, 131, 47, 226);
+  static Color labourService = const Color.fromARGB(255, 13, 141, 245);
+  static Color iconOnSelectColor = const Color(0xFFffffff);
+  static Color iconOnUnselectColor = const Color.fromARGB(255, 240, 119, 169);
+  static Color textColor = const Color.fromARGB(255, 241, 135, 188);
+
+}
