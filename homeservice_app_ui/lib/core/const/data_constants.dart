ebfb51a6388b15550de2bb14a1d2fb90c28c6800
mobisa-012 <mobisakwamboka@gmail.com>
2022-08-31 09:44:03 +0300

    auth service

--- homeservice_app_ui/lib/core/const/data_constants.dart
+++ homeservice_app_ui/lib/core/const/data_constants.dart
@@ -1,5 +1,5 @@
-import 'package:homeservice_app_ui/core/path_constants.dart';
-import 'package:homeservice_app_ui/core/text_constants.dart';
+import 'package:homeservice_app_ui/core/const/path_constants.dart';
+import 'package:homeservice_app_ui/core/const/text_constants.dart';
 import 'package:homeservice_app_ui/screens/onboarding/widgets/onboarding_tile.dart';
 
 class DataConstants {
--- homeservice_app_ui/lib/main.dart
+++ homeservice_app_ui/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:homeservice_app_ui/core/color.dart';
+import 'package:homeservice_app_ui/core/const/color.dart';
 import 'package:homeservice_app_ui/screens/onboarding/pages/onboarding_page.dart';
 import 'package:homeservice_app_ui/screens/tab_bar/pages/tab_bar_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized;
-  await Firebase.initializeApp();
   runApp( const MyApp());
 }
 
