4b9b01302b73165da5453f5d3838c7a2dc7903c7
mobisa-012 <mobisakwamboka@gmail.com>
2022-09-12 10:35:22 +0300

    added notification plugin

--- homeservice_app_ui/lib/main.dart
+++ homeservice_app_ui/lib/main.dart
@@ -2,9 +2,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:homeservice_app_ui/core/const/color.dart';
 import 'package:homeservice_app_ui/screens/onboarding/pages/onboarding_page.dart';
 import 'package:homeservice_app_ui/screens/tab_bar/pages/tab_bar_page.dart';
+
+import 'core/services/notification_service.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,21 +19,57 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  static final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      NotificationService.flutterLocalNotificationsPlugin;
+
+  @override
+  initState() {
+    super.initState();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    IOSInitializationSettings initializationSettingsIOS =
+        const IOSInitializationSettings();
+    final InitializationSettings initializationSettings =
+        InitializationSettings(
+            android: initializationSettingsAndroid,
+            iOS: initializationSettingsIOS);
+
+    tz.initializeTimeZones();
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: selectNotification);
+  }
+
   Widget build(BuildContext context) {
     final isLoggedIn = FirebaseAuth.instance.currentUser != null;
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Tasky',
-        theme: ThemeData(
-            textTheme:
-                TextTheme(bodyText1: TextStyle(color: AppColors.textColor)),
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-            scaffoldBackgroundColor: Colors.white),
-        home: isLoggedIn ? const TabBarPage() : const OnboardingPage());
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tasky',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
+      home: isLoggedIn ? const TabBarPage() : const OnboardingPage(),
+    );
+  }
+
+  Future selectNotification(String? payload) async {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: const Text('Payload'),
+          content: Text('Payload : $payload'),
+        );
+      },
+    );
   }
 }
